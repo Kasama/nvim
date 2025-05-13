906661b101cd6d5d7e841f018642aa4aceb521a5
Kasama <robertoaall@gmail.com>
2025-05-13 17:59:56 -0300

    comment out img-clip

--- lua/kasama/plugins/llm-coding.lua
+++ lua/kasama/plugins/llm-coding.lua
@@ -53,23 +53,23 @@ return {
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         "zbirenbaum/copilot.lua",      -- for providers='copilot'
         'MeanderingProgrammer/render-markdown.nvim',
-        {
-          -- support for image pasting
-          "HakonHarnes/img-clip.nvim",
-          event = "VeryLazy",
-          opts = {
-            -- recommended settings
-            default = {
-              embed_image_as_base64 = false,
-              prompt_for_file_name = false,
-              drag_and_drop = {
-                insert_mode = true,
-              },
-              -- required for Windows users
-              use_absolute_path = true,
-            },
-          },
-        },
+        -- {
+        --   -- support for image pasting
+        --   "HakonHarnes/img-clip.nvim",
+        --   event = "VeryLazy",
+        --   opts = {
+        --     -- recommended settings
+        --     default = {
+        --       embed_image_as_base64 = false,
+        --       prompt_for_file_name = false,
+        --       drag_and_drop = {
+        --         insert_mode = true,
+        --       },
+        --       -- required for Windows users
+        --       use_absolute_path = true,
+        --     },
+        --   },
+        -- },
       },
       config = function()
         local avante = require('avante')
